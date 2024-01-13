d3bbbd47a1949118e4d0f83e521b324b5c060f1e
beomsuyoo <beomsuyoo@gmail.com>
2024-01-13 14:52:53 +0900

    boj 1966: Simulation + Queue

--- Backtracking/2580.cpp
+++ Backtracking/2580.cpp
@@ -1,1 +1,59 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int map[10][10];
+int row[10][10];
+int col[10][10];
+int sqr[10][10];
+vector<pair<int,int> >zeros;
+
+void btk(int z){
+    if(z==zeros.size()){
+        for (int i=0;i<9;i++){
+            for (int j=0;j<9;j++){
+                cout << map[i][j] << " ";
+            }
+            cout << '\n';
+        }
+        exit(0);
+    }
+
+    int x = zeros[z].first;
+    int y = zeros[z].second;
+    for (int i=1;i<10;i++){
+        if (col[x][i]==0 && row[y][i]==0 && sqr[(x/3)*3+(y/3)][i]==0){ // pruning
+            map[y][x]=i;
+            row[y][i]=1;
+            col[x][i]=1;
+            sqr[(x/3)*3+(y/3)][i]=1;
+            btk(z+1);
+            map[y][x]=0;
+            row[y][i]=0;
+            col[x][i]=0;
+            sqr[(x/3)*3+(y/3)][i]=0;
+        }
+    }
 }
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    for (int i=0;i<9;i++){
+        for (int j=0;j<9;j++){
+            cin >> map[i][j];
+            if (map[i][j]!=0){
+                row[i][map[i][j]]=1; // record numbers in each row
+                col[j][map[i][j]]=1; // record numbers in each coloum
+                sqr[(j/3)*3+(i/3)][map[i][j]]=1; // record numbers in each quadrant(0~8)
+            } else {
+                zeros.push_back({j,i});
+            }
+        }
+    }
+    btk(0);
+}
+
+/* 
+    N = |zeros|
+    O(2^N)
+*/
---
+++ Simulation/1966.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+int main(){
+    int t,n,q;
+    cin >> t;
+    while(t--){
+        deque<pair<int,int>>que;
+        cin >> n >> q;
+        for(int i=0;i<n;i++){
+            int a;
+            cin >> a;
+            que.push_back(make_pair(i,a));
+        }
+
+        int order = 0;
+
+        while(!que.empty()){
+            int cIndex = que.front().first;
+            int cPriority = que.front().second;
+            bool skip = false;
+
+            for(int i=1;i<que.size();i++){
+                if(que[i].second>cPriority){
+                   que.pop_front();
+                   que.push_back(make_pair(cIndex,cPriority));
+                   skip=true;
+                   break;
+               }
+            }
+
+           if(skip==false){
+               int pop = que.front().first;
+               que.pop_front();
+               order++;
+                //cout << pop << endl;
+                if(pop==q){
+                    cout << order << '\n';
+                }
+            }
+        }
+    }
+}
+
+/* 
+    Approach:
+        Iterate the queue everytime to check if there are higher priorities behind the front
+    Time Complexity:
+        O(N^2)
+*/
