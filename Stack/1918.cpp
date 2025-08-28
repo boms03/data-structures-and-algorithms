7210f08eef1a0e4da1c4570295bf00e4fc60a6d5
boms03 <beomsuyoo@gmail.com>
2025-08-28 10:51:34 +0900

    boj 1918: stack

---
+++ Stack/1918.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int getPriority(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0; 
+}
+
+int main() {
+    stack<char> stk;
+    string s;
+    cin >> s;
+    
+    for(int i = 0; i < s.length(); i++) {
+        char c = s[i];
+        
+        if('A' <= c && c <= 'Z') {
+            cout << c;
+        }
+        else {
+            if(c == ')') {
+                while(!stk.empty() && stk.top() != '(') {
+                    cout << stk.top();
+                    stk.pop();
+                }
+                if(!stk.empty()) stk.pop();
+            }
+            else if(c == '(') {
+                stk.push(c);
+            }
+            else {
+                while(!stk.empty() && stk.top() != '(' && 
+                      getPriority(stk.top()) >= getPriority(c)) {
+                    cout << stk.top();
+                    stk.pop();
+                }
+                stk.push(c);
+            }
+        }
+    }
+    
+    while(!stk.empty()) {
+        cout << stk.top();
+        stk.pop();
+    }
+    return 0;
+}
